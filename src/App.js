07250ec644b330345acf9eb10e047bb3c5655c11
jmpeoples <jmpeoples.88@gmail.com>
2020-05-05 13:57:32 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,67 @@ import React from 'react';
 import './App.css';
 import Kart from './components/kart';
 import CartsInput from './components/CartsInput';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   return (
     //<CartsInput />
-    <Kart />
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Kart />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
+
+/*function Home() {
+  return <h2>Home</h2>;
+}
+
+
+function About() {
+  return <h2>About</h2>;
+}
+*/
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
 // food store
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const About = () => {
+
+    return (
+        <Container>
+            <h1>Didn't melt fairer keepsakes since Fellowship elsewhere.</h1>
+            <p>Woodlands payment Osgiliath tightening. Barad-dur follow belly comforts tender tough bell? Many that live deserve death. Some that die deserve life. Outwitted teatime grasp defeated before stones reflection corset seen animals Saruman's call?</p>
+            <h2>Tad survive ensnare joy mistake courtesy Bagshot Row.</h2>
+            <p>Ligulas step drops both? You shall not pass! Tender respectable success Valar impressive unfriendly bloom scraped? Branch hey-diddle-diddle pony trouble'll sleeping during jump Narsil.</p>
+            <h3>North valor overflowing sort Iáve mister kingly money?</h3>
+            <p>Curse you and all the halflings! Deserted anytime Lake-town burned caves balls. Smoked lthilien forbids Thrain?</p>
+            <ul>
+                <li>Adamant.</li>
+                <li>Southfarthing!</li>
+                <li>Witch-king.</li>
+                <li>Precious.</li>
+                <li>Gaffer's!</li>
+            </ul>
+
+        </Container>
+    )
+}
+
+export default About;
